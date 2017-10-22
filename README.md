b28735f05abf91e32af68391e1e0d3c3f31455fe
郑小川 <245346632@163.com>
2017-10-22 21:13:20 +0800

    1
    daily code

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # Code
+daily code
+
+## HelloWorld程序
+```
+using System;<br>
+namespace Helloworld<br>
+{    <br>
+class Program    <br>
+{     <br>
+static void Main(string[] args)<br>
+{  <br>
+Console.WriteLine("Hello World!");    <br>  
+}    <br>
+} <br>
+}<br>
+```
+## 窗体： 按钮单击事件改编文本框的内容
+```
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "等于二";
+        }
+    }
+}
+```
+###显示结果如下：
+
+![](./imgs/壹加壹等于几.jpg)
+https://github.com/xiaochuanzheng/Code/tree/master/images/壹加壹等于几.jpg
+
+![](./imgs/等于二.jpg)
+https://github.com/xiaochuanzheng/Code/tree/master/images/等于二.jpg
+
+## 窗体： 按钮单击事件弹出另一个窗体
+```
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+    }
+}
+```
+###显示结果如下：
+
+![](./imgs/变出另一个按钮.jpg)
+https://github.com/xiaochuanzheng/Code/tree/master/images/变出另一个按钮.jpg
+
+![](./imgs/新打开的窗体.jpg)
+https://github.com/xiaochuanzheng/Code/tree/master/images/新打开的窗体.jpg
+
---
+++ work2.2/work2.2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace work2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 13F);
+            this.button1.Location = new System.Drawing.Point(44, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "变出另一个按钮";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("宋体", 13F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(27, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "想不想看另一个窗体呀";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
